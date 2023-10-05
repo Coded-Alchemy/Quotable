0505c39590075136ef42883fdde6872390549a46
Taji Abdullah <tajiabdullah1@gmail.com>
2023-10-05 04:06:32 -0400

    Fix AuthorTest test.

--- database/src/test/java/coded/alchemy/qoutable/database/data/AuthorTest.kt
+++ database/src/test/java/coded/alchemy/qoutable/database/data/AuthorTest.kt
@@ -11,10 +11,10 @@ import org.junit.Test
 class AuthorTest {
     @Test
     fun `test author instantiation`() {
-        val author = Author(authorId = 456, name = "Test Driven", slug = "test_driven")
+        val author = Author(authorId = 456L, name = "Test Driven", slug = "test_driven")
 
         Assert.assertNotNull(author)
-        Assert.assertEquals(456, author.authorId)
+        Assert.assertEquals(456L, author.authorId)
         Assert.assertEquals("Test Driven", author.name)
         Assert.assertEquals("test_driven", author.slug)
     }
