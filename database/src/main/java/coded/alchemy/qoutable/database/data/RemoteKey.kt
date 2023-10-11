d963b4753dc536193fbe9e006ce3e4b62bfce293
Taji Abdullah <tajiabdullah1@gmail.com>
2023-10-11 12:26:34 -0400

    Add Primary key.

--- database/src/main/java/coded/alchemy/qoutable/database/data/RemoteKey.kt
+++ database/src/main/java/coded/alchemy/qoutable/database/data/RemoteKey.kt
@@ -1,6 +1,9 @@
 package coded.alchemy.qoutable.database.data
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "remote_keys")
-data class RemoteKey(val label: String, val nextKey: Int?)
+data class RemoteKey(
+    @PrimaryKey val label: String,
+    val nextKey: Int?)
