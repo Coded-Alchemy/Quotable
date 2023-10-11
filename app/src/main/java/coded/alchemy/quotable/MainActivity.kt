ff3b3295005d27580cfe39f561f41199d29c2e4c
Taji Abdullah <tajiabdullah1@gmail.com>
2023-10-11 17:07:18 -0400

    Display test info from viewModel

--- app/src/main/java/coded/alchemy/quotable/MainActivity.kt
+++ app/src/main/java/coded/alchemy/quotable/MainActivity.kt
@@ -3,6 +3,7 @@ package coded.alchemy.quotable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,11 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import coded.alchemy.quotable.compose.quoteList.QuoteListScreen
 import coded.alchemy.quotable.ui.theme.QuotableTheme
+import coded.alchemy.quotable.viewModel.QuoteListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: QuoteListViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -24,28 +28,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting(getString(R.string.app_name))
+                    QuoteListScreen(viewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuotableTheme {
-        Greeting("Android")
     }
 }
