5d00db24877e868fcdd141f9ef3cca4a0713b1c1
Taji Abdullah <tajiabdullah1@gmail.com>
2023-10-12 10:48:37 -0400

    Add QuoteTest.

---
+++ database/src/test/java/coded/alchemy/qoutable/database/data/QuoteTest.kt
@@ -0,0 +1,32 @@
+package coded.alchemy.qoutable.database.data
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class QuoteTest {
+    @Test
+    fun testQuoteProperties() {
+        // Arrange
+        val id = "123"
+        val author = "John Doe"
+        val content = "This is a sample quote."
+        val tags = listOf("tag1", "tag2")
+        val authorSlug = "john-doe"
+        val length = 42
+        val dateAdded = "2023-01-15"
+        val dateModified = "2023-01-20"
+
+        // Act
+        val quote = Quote(id, author, content, tags, authorSlug, length, dateAdded, dateModified)
+
+        // Assert
+        assertEquals(id, quote._id)
+        assertEquals(author, quote.author)
+        assertEquals(content, quote.content)
+        assertEquals(tags, quote.tags)
+        assertEquals(authorSlug, quote.authorSlug)
+        assertEquals(length, quote.length)
+        assertEquals(dateAdded, quote.dateAdded)
+        assertEquals(dateModified, quote.dateModified)
+    }
+}
