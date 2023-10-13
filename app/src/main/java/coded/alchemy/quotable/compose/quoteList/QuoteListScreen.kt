3eab7906f481efd9ae83b484c592d93509d3964d
Taji Abdullah <tajiabdullah1@gmail.com>
2023-10-13 16:19:09 -0400

    Add top and bottom app bars with crash...

--- app/src/main/java/coded/alchemy/quotable/compose/quoteList/QuoteListScreen.kt
+++ app/src/main/java/coded/alchemy/quotable/compose/quoteList/QuoteListScreen.kt
@@ -1,19 +1,31 @@
 package coded.alchemy.quotable.compose.quoteList
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,13 +42,62 @@ const val TAG = "QuoteListScreen"
 fun QuoteListScreen(viewModel: QuoteListViewModel = hiltViewModel()) {
     val articleList = viewModel.getFlow().collectAsLazyPagingItems()
 
-    Log.d(TAG, "QuoteListScreen: $articleList")
-
+//    Log.d(TAG, "QuoteListScreen: $articleList")
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        QuoteList(articleList = articleList)
+        Scaffolding(articleList)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Scaffolding(articleList: LazyPagingItems<Quote>) {
+    var presses by remember { mutableIntStateOf(0) }
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+//                colors = topAppBarColors(
+//                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+//                    titleContentColor = MaterialTheme.colorScheme.primary,
+//                ),
+                title = {
+                    Text(
+                        "Quotable",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary,
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = "Bottom app bar",
+                )
+            }
+        },
+//        floatingActionButton = {
+//            FloatingActionButton(onClick = { presses++ }) {
+//                Icon(Icons.Default.Add, contentDescription = "Add")
+//            }
+//        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            QuoteList(articleList)
+        }
     }
 }
 
@@ -69,21 +130,12 @@ fun QuoteListItem(quoteEntity: Quote) {
     }
 }
 
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuotableTheme {
-        Greeting("Android")
-    }
-}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    QuotableTheme {
+//        Scaffolding()
+//    }
+//}
