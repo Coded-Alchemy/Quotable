2df743ce077fd236166d99fdda0612e4c6967021
Taji Abdullah <tajiabdullah1@gmail.com>
2023-10-31 11:36:01 -0400

    Run kt lint.

--- app/src/main/java/coded/alchemy/quotable/compose/QuotableApp.kt
+++ app/src/main/java/coded/alchemy/quotable/compose/QuotableApp.kt
@@ -11,22 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import coded.alchemy.quotable.R
-import coded.alchemy.quotable.compose.authorList.AuthorListScreen
-import coded.alchemy.quotable.compose.quoteDetail.QuoteDetailScreen
-import coded.alchemy.quotable.compose.quoteList.QuoteListScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
