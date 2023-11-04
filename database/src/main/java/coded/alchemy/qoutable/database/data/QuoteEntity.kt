1be973f4ab70d414090c79034a9bd0b5357746f1
Taji Abdullah <tajiabdullah1@gmail.com>
2023-11-04 11:28:20 -0400

    Make QuoteEntity use vars.

--- database/src/main/java/coded/alchemy/qoutable/database/data/QuoteEntity.kt
+++ database/src/main/java/coded/alchemy/qoutable/database/data/QuoteEntity.kt
@@ -23,11 +23,11 @@ import androidx.room.PrimaryKey
 data class QuoteEntity(
     @PrimaryKey
     @ColumnInfo(name = "quoteId")
-    val quoteId: String = "",
-    @ColumnInfo(name = "authorId") val authorId: Long = Long.MIN_VALUE,
-    @ColumnInfo(name = "content") val content: String = "",
-    @ColumnInfo(name = "author_slug") val author_slug: String = "",
-    @ColumnInfo(name = "length") val length: Long = Long.MIN_VALUE,
-    @ColumnInfo(name = "date_added") val date_added: String = "",
-    @ColumnInfo(name = "date_modified") val date_modified: String = ""
+    var quoteId: String = "",
+    @ColumnInfo(name = "authorId") var authorId: Long = Long.MIN_VALUE,
+    @ColumnInfo(name = "content") var content: String = "",
+    @ColumnInfo(name = "author_slug") var author_slug: String = "",
+    @ColumnInfo(name = "length") var length: Long = Long.MIN_VALUE,
+    @ColumnInfo(name = "date_added") var date_added: String = "",
+    @ColumnInfo(name = "date_modified") var date_modified: String = ""
 )
